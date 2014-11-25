92edc3d850721f6c3bc18ea20e7bc51c4c329cdf
cemm <eduardo@mg5.us>
2014-11-24 23:22:55 -0500

    Add Show Position

--- www/js/index.js
+++ www/js/index.js
@@ -52,15 +52,33 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
 
-    function displayLatLong(pos) {
-        $('.lat-view').html(pos.coords.latitude);
-        $('.long-view').html(pos.coords.longitude);
+    function displayLatLong(position) {
+        $('.lat-view').html(position.coords.latitude);
+        $('.long-view').html(position.coords.longitude);
 
-        var latlon = pos.coords.latitude + "," + pos.coords.longitude;
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    latlon = new google.maps.LatLng(lat, lon)
+    mapholder = document.getElementById('mapholder')
+    mapholder.style.height = '250px';
+    mapholder.style.width = '500px';
 
-        var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
-            +latlon+"&zoom=14&size=400x300&sensor=false";
-            document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+    var myOptions = {
+    center:latlon,zoom:14,
+    mapTypeId:google.maps.MapTypeId.ROADMAP,
+    mapTypeControl:false,
+    navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+    }
+    
+//    var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
+//    var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});        
+//        
+//        
+//        var latlon = pos.coords.latitude + "," + pos.coords.longitude;
+//
+//        var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+//            +latlon+"&zoom=14&size=400x300&sensor=false";
+//            document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
     }
 
     $('#getIt').click(function() {
